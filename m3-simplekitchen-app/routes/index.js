542b6418c7a2767fbf216a69924c2035a5b32cc8
xu chen <xuchen@xus-macbook-pro.lan>
2022-07-23 14:27:05 -0700

    in lab

--- m3-simplekitchen-app/routes/index.js
+++ m3-simplekitchen-app/routes/index.js
@@ -1,57 +1,85 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const auth = require('http-auth');
-const { check, validationResult } = require('express-validator');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const auth = require("http-auth");
+const { check, validationResult } = require("express-validator");
 
 const router = express.Router();
-const Registration = mongoose.model('Registration');
+const Registration = mongoose.model("Registration");
 const basic = auth.basic({
-  file: path.join(__dirname, '../users.htpasswd'),
+  file: path.join(__dirname, "../users.htpasswd"),
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   //res.send('It works!');
-  res.render('index', { title: 'Registration form' });
+  res.render("index", { title: "home page", path: req.url });
 });
 
-router.get('/registrations', basic.check((req, res) => {
-  Registration.find()
-    .then((registrations) => {
-      res.render('registrants', { title: 'Listing registrations', registrations });
-    })
-    .catch(() => { 
-      res.send('Sorry! Something went wrong.'); 
-    });
-}));
+router.get("/register", (req, res) => {
+  //res.send('It works!');
+  res.render("index", { title: "register page", path: req.url });
+});
 
-router.post('/', 
-    [
-        check('name')
-        .isLength({ min: 1 })
-        .withMessage('Please enter a name'),
-        check('email')
-        .isLength({ min: 1 })
-        .withMessage('Please enter an email'),
-    ],
-    (req, res) => {
-        //console.log(req.body);
-        const errors = validationResult(req);
-        if (errors.isEmpty()) {
-          const registration = new Registration(req.body);
-          registration.save()
-            .then(() => {res.send('Thank you for your registration!');})
-            .catch((err) => {
-              console.log(err);
-              res.send('Sorry! Something went wrong.');
-            });
-          } else {
-            res.render('form', { 
-                title: 'Registration form',
-                errors: errors.array(),
-                data: req.body,
-             });
-          }
-    });
+router.get(
+  "/registrations",
+  basic.check((req, res) => {
+    Registration.find()
+      .then((registrations) => {
+        res.render("registrants", {
+          title: "Listing registrations",
+          registrations,
+        });
+      })
+      .catch(() => {
+        res.send("Sorry! Something went wrong.");
+      });
+  })
+);
+
+router.get("/thankyou", (req, res) => {
+  res.render("index", {
+    title: "thankyou page",
+    path: req.url,
+    content: "Thank you for your registration!"
+  });
+});
+
+router.get("/dberror", (req, res) => {
+  res.render("index", {
+    title: "error page",
+    path: req.url,
+    content: "Sorry! Something went wrong."
+  });
+});
+
+router.post(
+  "/register",
+  [
+    check("name").isLength({ min: 1 }).withMessage("Please enter a name"),
+    check("email").isLength({ min: 1 }).withMessage("Please enter an email"),
+  ],
+  (req, res) => {
+    //console.log(req.body);
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      const registration = new Registration(req.body);
+      registration
+        .save()
+        .then(() => {
+          res.redirect("/thankyou");
+        })
+        .catch((err) => {
+          res.redirect("/dberror");
+        });
+    } else {
+      res.render("index", {
+        title: "register page",
+        path: req.url,
+        errors: errors.array(),
+        data: req.body,
+      });
+    }
+  }
+);
 
 module.exports = router;
